a28cf2b90f45da7a56f6219c455101fcc3b14633
Neutron35 <steve.lieuron@gmail.com>
2024-07-15 09:09:19 +0200

    feat: use JSON data, Banner & Thumbnail components on Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,17 @@
-import picture from '../../assets/landscape.jpg';
+import datas from '../../assets/data.json';
+import homePicture from '../../assets/homePicture.jpg';
+import Banner from '../../components/Banner';
+import Thumbnail from '../../components/Thumbnail';
 import './home.scss';
 
 function Home() {
   return (
     <main>
-      <section>
-        <img src={picture} alt="" />
-        <div>Chez vous, partout et ailleurs</div>
+      <Banner picture={homePicture} text={'Chez vous, partout et ailleurs'} />
+      <section id="gallery">
+        {datas.map((data) => (
+          <Thumbnail key={data.id} title={data.title} cover={data.cover} />
+        ))}
       </section>
     </main>
   );
