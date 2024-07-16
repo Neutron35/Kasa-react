d5dc4097b7d1a7e644d70503e5e7c547b74e3d88
Neutron35 <steve.lieuron@gmail.com>
2024-07-16 18:58:22 +0200

    feat: Lodging page now redirects to 404 if lodging's ID doesn't exist

--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import backendData from '../../assets/backend-data.json';
+
 function Lodging() {
-  return <div>Fiche logement</div>;
+  const { lodgingId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const idFound = backendData.some((data) => data.id === lodgingId);
+    if (!idFound) {
+      navigate('/notfound', { replace: true });
+    }
+  }, [lodgingId, navigate]);
+
+  return <div>Fiche logement {lodgingId}</div>;
 }
 
 export default Lodging;
