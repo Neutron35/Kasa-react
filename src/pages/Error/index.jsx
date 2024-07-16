2e17e2d95d001f741a88b7afa1a38209bfdd1b92
Neutron35 <steve.lieuron@gmail.com>
2024-07-16 15:29:55 +0200

    feat: created 404 page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+import './error.scss';
+
 function Error() {
-  return <div>Page 404</div>;
+  return (
+    <main className="error">
+      <p className="error__code">404</p>
+      <p className="error__details">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error__homelink">
+        Retourner sur la page d'accueil
+      </Link>
+    </main>
+  );
 }
 
 export default Error;
