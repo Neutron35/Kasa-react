82788879fbdd50d98130fdaddb012e4887d53837
Neutron35 <steve.lieuron@gmail.com>
2024-07-17 09:41:23 +0200

    fix: simplified props to pass lodging's data

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import './card.scss';
 
-function Card({ title, cover, data }) {
+function Card({ data }) {
   const path = `/lodging/${data.id}`;
   return (
     <Link to={path}>
       <figure className="card">
-        <img src={cover} alt={title} className="card__cover"></img>
-        <figcaption className="card__title">{title}</figcaption>
+        <img src={data.cover} alt={data.title} className="card__cover"></img>
+        <figcaption className="card__title">{data.title}</figcaption>
       </figure>
     </Link>
   );
