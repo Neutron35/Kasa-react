4a523aebb1b54ff87492085c33d78410e7511dd8
Neutron35 <steve.lieuron@gmail.com>
2024-07-18 10:21:37 +0200

    fix: removed path const

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 import './card.scss';
 
 function Card({ data }) {
-  const path = `/lodging/${data.id}`;
   return (
-    <Link to={path}>
+    <Link to={`/lodging/${data.id}`}>
       <figure className="card">
         <img src={data.cover} alt={data.title} className="card__cover"></img>
         <figcaption className="card__title">{data.title}</figcaption>
