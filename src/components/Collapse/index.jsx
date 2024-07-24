3efa3d1f7834c8caabcf26b0e8117e8e88cecbf7
Neutron35 <steve.lieuron@gmail.com>
2024-07-24 10:29:26 +0200

    fix: change tags used in Collapse's content if content is an array or not

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -2,26 +2,27 @@ import './collapse.scss';
 import { useState } from 'react';
 import arrowIcon from '../../assets/arrow-back-ios-up.svg';
 
-function Collapse({ title, content }) {
+function Collapse({ title, content, page }) {
   const [isOpen, setIsOpen] = useState(false);
+  const isContentArray = Array.isArray(content);
   return isOpen ? (
-    <div className="collapse">
-      <div className="collapse__title">
+    <div className={`collapse collapse--${page}`}>
+      <div className={`collapse__title collapse__title--${page}`}>
         <span>{title}</span>
         <img src={arrowIcon} className="arrow-icon open" alt="" onClick={() => setIsOpen(false)}></img>
       </div>
       <div className="collapse__content collapse__content--open">
-        <p>{content}</p>
+        <ul>{isContentArray ? content.map((item, index) => <li key={index}>{item}</li>) : <p>{content}</p>}</ul>
       </div>
     </div>
   ) : (
-    <div className="collapse">
-      <div className="collapse__title">
+    <div className={`collapse collapse--${page}`}>
+      <div className={`collapse__title collapse__title--${page}`}>
         <span>{title}</span>
         <img src={arrowIcon} className="arrow-icon" alt="" onClick={() => setIsOpen(true)}></img>
       </div>
       <div className="collapse__content collapse__content--closed">
-        <p>{content}</p>
+        <ul>{isContentArray ? content.map((item, index) => <li key={index}>{item}</li>) : <p>{content}</p>}</ul>
       </div>
     </div>
   );
