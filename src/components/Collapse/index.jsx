41386e5c788011fa2664eed5f33011bad7f6b5a0
Neutron35 <steve.lieuron@gmail.com>
2024-07-24 16:02:22 +0200

    fix: some optimizations in code

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -5,23 +5,19 @@ import arrowIcon from '../../assets/arrow-back-ios-up.svg';
 function Collapse({ title, content }) {
   const [isOpen, setIsOpen] = useState(false);
   const isContentArray = Array.isArray(content);
-  return isOpen ? (
+
+  return (
     <div className="collapse">
       <div className="collapse__title">
         <span>{title}</span>
-        <img src={arrowIcon} className="arrow-icon open" alt="" onClick={() => setIsOpen(false)}></img>
+        <img
+          src={arrowIcon}
+          className={`arrow-icon ${isOpen ? 'open' : ''}`}
+          alt=""
+          onClick={() => setIsOpen(!isOpen)}
+        ></img>
       </div>
-      <div className="collapse__content collapse__content--open">
-        <ul>{isContentArray ? content.map((item, index) => <li key={index}>{item}</li>) : <p>{content}</p>}</ul>
-      </div>
-    </div>
-  ) : (
-    <div className="collapse">
-      <div className="collapse__title">
-        <span>{title}</span>
-        <img src={arrowIcon} className="arrow-icon" alt="" onClick={() => setIsOpen(true)}></img>
-      </div>
-      <div className="collapse__content collapse__content--closed">
+      <div className={`collapse__content ${isOpen ? 'collapse__content--open' : 'collapse__content--closed'}`}>
         <ul>{isContentArray ? content.map((item, index) => <li key={index}>{item}</li>) : <p>{content}</p>}</ul>
       </div>
     </div>
