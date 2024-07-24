2e0087eb13d3740fc908a76bbabe8bee77828410
Neutron35 <steve.lieuron@gmail.com>
2024-07-24 10:25:13 +0200

    fix: added className to logo & NavLinks are now in a list

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,14 +5,20 @@ import './header.scss';
 function Header() {
   return (
     <header>
-      <img src={logo} alt="Kasa" />
+      <img src={logo} alt="Kasa" className="header__logo" />
       <nav className="navbar">
-        <NavLink to="/" activeclassname="active" className="navbar__link">
-          Accueil
-        </NavLink>
-        <NavLink to="/about" activeclassname="active" className="navbar__link">
-          A propos
-        </NavLink>
+        <ul>
+          <li>
+            <NavLink to="/" activeclassname="active" className="navbar__link">
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeclassname="active" className="navbar__link">
+              A propos
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
