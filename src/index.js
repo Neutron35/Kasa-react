b99a9d6b780895dee0a17993bf90338b13cf76d8
Neutron35 <steve.lieuron@gmail.com>
2024-08-06 09:13:32 +0200

    fix: sorted react-router-dom imports for better readability

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import Home from './pages/Home';
 import About from './pages/About';
